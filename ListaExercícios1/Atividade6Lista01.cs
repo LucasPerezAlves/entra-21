d1ef8e261ce3665e4f310a582295dda4f254d621
Lucas Perez <perezcfnpbnu@gmail.com>
2022-05-15 23:50:53 -0300

    Adicionado a Atividade 6 da lista 1

---
+++ ListaExercícios1/Atividade6Lista01.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.AtividadeLista1
+{
+    internal class Atividade6Lista01
+    {
+        public void Executar()
+        {
+            //            6.Calcule a quantidade de litros de água ingeridos em determinado período e o valor pago.
+            //            Para tal deve - se solicitar ao usuário a média da quantidade de litros ingeridos por dia,
+            //            a quantidade de anos e o valor pago pela água.
+            //  Exemplo: 
+            //Quantidade de litros: 3
+            //Quantidade de anos consumidos: 17
+            //Valor por Litro: R$ 9.75
+            //Quantidade de Litros consumidos: 18.615 L
+            // Valor Total Pago: R$ 181.496, 25
+
+
+            Console.Write("Digite a quantidade média de litros ingeridos por dia: ");
+            int litrosAgua = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Por quantos anos você ingeriu essa média de litros: ");
+            int anosConsumidos = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o valor pago pela água: $ ");
+            double valorPorLitro = Convert.ToDouble(Console.ReadLine());
+
+            double litrosConsumidos = litrosAgua * anosConsumidos;
+            double valorTotalPago = valorPorLitro * anosConsumidos;
+
+            Console.WriteLine("A quantidade de litros consumidos é: " + (litrosAgua * anosConsumidos));
+            Console.Write("O valor total pago é: $ " + (valorPorLitro * anosConsumidos));
+
+        }
+    }
+}
--- ListaExercícios1/Program.cs
+++ ListaExercícios1/Program.cs
@@ -4,7 +4,9 @@ Console.Write(@"--------------MENU---------------
 1 - Atividade 01
 2 - Atividade 02
 3 - Atividade 03
-4 - Atividade 04");
+4 - Atividade 04
+5 - Atividade 05
+6 - Atividade 06");
 
 Console.Write("Digite a opção desejada: ");
 int opcaoDesejada = Convert.ToInt32(Console.ReadLine());
@@ -22,5 +24,20 @@ else if (opcaoDesejada == 2)
 else if (opcaoDesejada == 3)
 {
     Atividade3Lista01 nossaAtividade = new Atividade3Lista01();
+    nossaAtividade.Executar();
+}
+else if (opcaoDesejada == 4)
+{
+    Atividade4Lista01 nossaAtividade = new Atividade4Lista01();
+    nossaAtividade.Executar();
+}
+else if (opcaoDesejada == 5)
+{
+    Atividade5Lista01 nossaAtividade = new Atividade5Lista01();
+    nossaAtividade.Executar();
+}
+else if (opcaoDesejada == 6)
+{
+    Atividade6Lista01 nossaAtividade = new Atividade6Lista01();
     nossaAtividade.Executar();
 }
