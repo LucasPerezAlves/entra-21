ab01896104fa6a4f32708906bd9eb5aded7df424
JulianaForbici <julianaforbicii@gmail.com>
2022-05-26 22:10:19 -0300

    Começo das atividades Orientação Objetos Foi começado a atividade 1 de Orientação e Objetos

---
+++ Entra21.AtividadesO.O/Exercício01/ExemploLivro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.AtividadesOrientacaoObjetos.Exercício01
+{
+    public class ExemploLivro
+    {
+       public void Executar()
+        {
+            var nomeTitulo = new Livro();
+            nomeTitulo.ApresentarTituloAutor();
+
+
+        }
+    }
+}
---
+++ Entra21.AtividadesO.O/Exercício01/Livro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.AtividadesOrientacaoObjetos
+{
+    public class Livro
+    {
+        public string Titulo;
+        public string Autor;
+        public string CodeIsbn;
+        public string idiomaOriginal;
+        public DateTime DataLancamento;
+        public int QuantidadePaginas;
+        public int QuantidadePaginasLidas;
+        public int QuantidadeReleituras;
+
+        public void ApresentarTituloAutor()
+        {
+            Console.WriteLine($"Nome do titulo é: {Titulo}" + 
+                $"\n O autor é:  {Autor}");
+            
+        }
+        
+        public int quantidadePaginasParaLer()
+        { 
+
+            Console.WriteLine($"A quantidade de páginas que você precisa ler é: {QuantidadePaginas - QuantidadePaginasLidas}");
+            QuantidadePaginasLidas = Convert.ToInt32(Console.ReadLine());
+
+            return QuantidadePaginasLidas;
+        }
+
+        public void QuantidadePaginasLidaTotal()
+        {
+            QuantidadeReleituras = QuantidadePaginasLidas + QuantidadePaginasLidas;
+           
+        }
+    }
+}
