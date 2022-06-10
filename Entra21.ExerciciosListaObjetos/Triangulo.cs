9766ee571847b56142647189b2dc5e286db11a1e
Lucas Perez Alves <perezcfnpbnu@gmail.com>
2022-06-09 22:06:57 -0300

    Começado o exercício do quadro Foi começado a realização do exercício do quadro passado em sala

---
+++ Entra21.ExerciciosListaObjetos/Triangulo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosListaObjetos
+{
+    public class Triangulo
+    {
+        public int lado1;
+        public int lado2;
+        public int lado3;
+
+        public bool ValidarTriangulo()
+        {
+            if ((lado1 + lado2) > lado3)
+            {
+                return true;
+            }
+            else if ((lado2 + lado3) > lado1)
+            {
+                return true;
+            }
+            else if ((lado3 + lado1) > lado2)
+            {
+                return true;
+            }
+
+
+
+
+
+        }
+    }
+}
--- ExemplosListasObjetos/ProdutoControlador.cs
+++ ExemplosListasObjetos/ProdutoControlador.cs
@@ -11,11 +11,150 @@ namespace Entra21.ExemplosListasObjetos
         private ProdutoServico produtoServico = new ProdutoServico();
         public void GerenciarMenu()
         {
-            Cadastrar();
-            ApresentarProdutos();
-            Cadastrar();
+            int codigo = 0;
+            //Repete enquanto o código não for o menu sair(6)
+            while (codigo != 6)
+            {
+                Console.Clear();
+
+                //Apresentar o menu e solicita o codigo
+                codigo = ApresentarSolicitarMenu();
+
+                Console.Clear();
+
+                if (codigo == 1)
+                {
+                    //Menu escolhido para listar produtos
+                    ApresentarProdutos();
+
+                }
+                else if (codigo == 2)
+                {
+                    //Menu escolhido para cadastrar produto
+                    Cadastrar();
+                }
+                else if (codigo == 3)
+                {
+                    //Menu escolhido para editar produto
+                    Editar();
+                }
+                else if (codigo == 4)
+                {
+                    Apagar();
+                }
+                else if (codigo == 5)
+                {
+                    ApresentarProduto();
+                }
+                Console.Write("\n\nDigite alguma tecla para continuar...");
+                Console.ReadKey();
+            }
+        }
+
+        private void ApresentarProduto()
+        {
+            ApresentarProduto();
+
+            Console.Write("Digite o código do produto a ser detalhado: ");
+            int codigo = Convert.ToInt32(Console.ReadLine());
+
+            var produto = produtoServico.ObterPorCodigo(codigo);
+
+            //Verifica se o produto não esta cadastrado na lista de produtos
+            if(produto == null)
+            {
+                Console.Write("Produto não cadastrado");
+                return;
+            }
+            Console.Clear();
+            Console.Write(@"Código: " + produto.Codigo +
+                "\nNome: " + produto.Nome +
+                "\nPreço unitário: " + produto.PrecoUnitario +
+                "\nQuantidade: " + produto.Quantidade +
+                "\nTotal: " + produto.CalcularPrecoTotal());
+               
+        }
+            private void Apagar()
+            {
+                ApresentarProdutos();
+
+                Console.Write("Digite o código do produto para apagar: ");
+                int codigo = Convert.ToInt32(Console.ReadLine());
+
+                var registroApagado = produtoServico.Apagar(codigo);
+
+                Console.Write(registroApagado == true
+                    ? "Registro removido com sucesso"
+                    : "Nenhum produto cadastrado com codigo informado");
+            }
+
+        private void Editar()
+        {
             ApresentarProdutos();
 
+            Console.Write("Código produto desejado: ");
+            var codigo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Nome: ");
+            var nome = Console.ReadLine();
+
+            Console.Write("Quantidade: ");
+            var quantidade = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Preço unitário: ");
+            var precoUnitario = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write(@"Localizações disponíveis: 
+- Armazem
+- Area Venda
+- Loja");
+            Console.Write("Localização: ");
+            var localizacao = Console.ReadLine();
+
+            var localizacaoProduto = ObterLocalizacaoProduto(localizacao);
+
+            var alterou = produtoServico.Editar(codigo, nome, precoUnitario, localizacaoProduto, quantidade);
+
+            if (alterou == false)
+            {
+                Console.Write("Código digitado não existe");
+
+            }
+            else
+            {
+                Console.Write("Produto alterado com sucesso");
+            }
+        }
+        private int ApresentarSolicitarMenu()
+        {
+            Console.WriteLine(@" Menu:
+1 - Listar todos
+2 - Cadastrar
+3 - Editar
+4 - Apagar
+5 - Apresentar produto desejado
+6 - Sair");
+
+            int codigo = SolicitarCodigo();
+            return codigo;
+        }
+        private int SolicitarCodigo()
+        {
+            int codigo = 0;
+            //Continua solicitando o código até que seja um código entre 1 e 6
+            while (codigo < 1 || codigo > 6)
+            {
+                try
+                {
+                    Console.Write("Digite o seu código");
+                    codigo = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.Write("Digite um menu válido");
+                }
+            }
+            return codigo;
         }
         private void Cadastrar()
         {
@@ -28,6 +167,12 @@ namespace Entra21.ExemplosListasObjetos
             Console.Write("Preço Unitário: ");
             var precoUnitario = Convert.ToDouble(Console.ReadLine());
 
+            Console.WriteLine(@"Localizações disponíveis: 
+- Armazem
+- Area Venda
+- Loja");
+
+
             Console.Write("Localização: ");
             var localizacao = Console.ReadLine();
 
@@ -35,7 +180,7 @@ namespace Entra21.ExemplosListasObjetos
 
             produtoServico.Adicionar(nome, precoUnitario, localizacaoProduto, quantidade);
         }
-        private ProdutoLocalizacao ObterLocalizacaoProduto (string localizacao)
+        private ProdutoLocalizacao ObterLocalizacaoProduto(string localizacao)
         {
             if (localizacao.ToLower() == "armazem")
             {
@@ -53,13 +198,22 @@ namespace Entra21.ExemplosListasObjetos
         private void ApresentarProdutos()
         {
             var produtos = produtoServico.ObterTodos();
-
-            for ( var i = 0; i < produtos.Count; i++)
+            if (produtos.Count == 0)
+            {
+                Console.WriteLine("Nenhum produto cadastrado :(");
+                //Para a execução deste método
+                return;
+            }
+            Console.WriteLine("Lista de Produtos");
+            for (var i = 0; i < produtos.Count; i++)
             {
                 var produtoAtual = produtos[i];
 
-                Console.Write("Nome: " + produtoAtual.Nome + "Preço unitário: " + produtoAtual.PrecoUnitario)
+                Console.Write(
+                    "\nCódigo: " + produtoAtual.Codigo +
+                    "\nNome: " + produtoAtual.Nome + "\n");
             }
         }
     }
 }
+
